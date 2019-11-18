644323c135d7ec60c2d6e31ed83e7ffbae1b71d9
jon <51472368+johnmwaura08@users.noreply.github.com>
2019-11-18 11:58:16 -0600

    updated todolist

--- todo-client/src/TodoList.js
+++ todo-client/src/TodoList.js
@@ -1,63 +1,132 @@
-import React from 'react'
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import { Done, ThreeDRotation } from '@material-ui/icons';
-import { Button } from '@material-ui/core';
-class TodoList extends React.Component{
-    constructor(props){
-        super(props);
+import React from "react";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import { Done, Add } from "@material-ui/icons";
+import { Button, Fab, TextField } from "@material-ui/core";
 
-        this.state = {todos: []}
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { todos: [], addingTodo: false, newTodoName: "" };
+  }
+
+  componentDidMount() {
+    this.getTodos();
+  }
+
+  getTodos = () => {
+    fetch("http://localhost:5000/api/todos")
+      .then(res => res.json())
+      .then(items => this.setState({ todos: items }));
+  };
+
+  addTodo = () => {
+    const url = "http://localhost:5000/api/todos";
+    const body = { name: this.state.newTodoName };
+    const response = fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(body)
+    });
+
+    response.then(() => {
+      this.setState({ newTodoName: "", addingTodo: false });
+      this.getTodos();
+    });
+  };
+
+  addRow() {
+    if (this.state.addingTodo) {
+      return (
+        <TableRow>
+          <TableCell component="th" scope="row">
+            <TextField
+              label="New Todo Item Name"
+              margin="normal"
+              value={this.state.newTodoName}
+              onChange={e => this.setState({ newTodoName: e.target.value })}
+            />
+          </TableCell>
+          <TableCell align="right">
+            <Button
+              onClick={() =>
+                this.setState({ addingTodo: false, newTodoName: "" })
+              }
+              variant="contained"
+            >
+              Cancel
+            </Button>
+            <Button
+              onClick={this.addTodo}
+              variant="contained"
+              color="secondary"
+            >
+              Add
+            </Button>
+          </TableCell>
+        </TableRow>
+      );
+    } else {
+      return <></>;
     }
+  }
 
-    componentDidMount(){
-        fetch('http://localhost:5000/api/todos').then(
-            res => res.json()
-        )
-        .then( items => this.setState({ todos: items}))
+  completeCell(completed) {
+    if (completed) {
+      return <Done />;
+    } else {
+      return (
+        <Button variant="contained" color="primary">
+          Complete Item
+        </Button>
+      );
     }
-    completeCell(completed){
-        if(completed){
-            return (<Done/>)
-        }else{
-            <Button variant="contained" color="primary"> Complete </Button>
-        }
-            
-        
-        return(
+  }
 
-        )
-    }
-    render(){
-
-        return (
-            <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Todo Name</TableCell>
-                <TableCell align="right">Completed</TableCell>
-                
+  render() {
+    return (
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>
+              Todo Name
+              <Fab
+                style={{ marginLeft: "6px" }}
+                size="small"
+                color="secondary"
+                aria-label="add"
+                onClick={() => this.setState({ addingTodo: true })}
+              >
+                <Add />
+              </Fab>
+            </TableCell>
+            <TableCell align="right">Completed</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <>
+            {this.addRow()}
+            {this.state.todos.map(todo => (
+              <TableRow key={todo._id}>
+                <TableCell component="th" scope="row">
+                  {todo.name}
+                </TableCell>
+                <TableCell align="right">
+                  {this.completeCell(todo.completed)}
+                </TableCell>
               </TableRow>
-            </TableHead>
-            <TableBody>
-              {this.state.todos.map(todo => (
-                <TableRow key={todo._id}>
-                  <TableCell component="th" scope="row">
-                  { todo.name}
-                  </TableCell>
-                  <TableCell align="right">{this.completeCell(todo.completed}</TableCell>
-                  
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-          <h1>{this.state.todos.length> 0 ? this.state.todos[0].name: "no todos"}</h1>
-        )
-    }
-  
+            ))}
+          </>
+        </TableBody>
+      </Table>
+    );
+  }
 }
 
 export default TodoList;
